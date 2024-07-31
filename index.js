a9bed383e87cc51ea16b4fcb0c5166213948d21c
shubhranshureact <raoaashish765+21@gmail.com>
2024-07-31 16:10:36 +0530

    cm3

--- index.js
+++ index.js
@@ -40,17 +40,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // }).promise();
 
 
+// var pool = mysql2.createPool({
+//     host: `wipsite.in`,
+//     user: `wipsite_startupkit`,
+//     password: `NWYV!(ymnNVU`,
+//     database: `wipsite_startupkit_react`,
+//     port: 3306,
+//     // multipleStatements : true,
+//     // connectionLimit: 10,
+//     // acquireTimeout: 30000,
+// });
 var pool = mysql2.createPool({
-    host: `wipsite.in`,
-    user: `wipsite_startupkit`,
-    password: `NWYV!(ymnNVU`,
+    host: `146.190.60.247`,
+    user: `admin`,
+    password: `47d511d7454a5747c7e54555aaeb2dc5d638ad6400ecf33c`,
     database: `wipsite_startupkit_react`,
     port: 3306,
-    multipleStatements : true,
-    connectionLimit: 10,
-    acquireTimeout: 30000,
+    // multipleStatements : true,
+    // connectionLimit: 10,
+    // acquireTimeout: 30000,
 });
-
 
 
 // let pool;
